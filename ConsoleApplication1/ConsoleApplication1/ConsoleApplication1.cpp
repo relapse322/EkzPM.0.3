9bde3ad57b1037ee51783661f293eb4d5f408a1f
stu-pkspk218 <stu-pkspk218@AMDVLA3511GW01>
2022-04-19 10:43:19 +0300

    __

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -6,7 +6,7 @@ struct Town
 
 {
 	char Name[30]; // название
-	int population; // население
+	int kolvo; // население
 	double S;   //площадь
 
 	void print()
@@ -14,19 +14,50 @@ struct Town
 	{
 
 		cout << "название: " << Name << endl;
-
 		cout << "количество населения: " << population << endl;
-
 		cout << "площадь: " << S << endl;
-
 
 	}
 
 };
 
 int main()
+{
+  setlocale(LC_ALL, "rus");
+
+  int size = 0;
+
+   cout << "Введите размер массива элементов Town: " << endl;
+
+   cin >> size;
+
+Town** A = new Town * [size];
+
+for (int i = 0; i < size; i++)
+
+{
+	A[i] = new Town;
+}
+for (int i = 0; i < size; i++)
+
+{
+
+	cout << "Введите название " << i + 1 << ":";
+
+	cin >> A[i]->Name;
+
+	cout << "Введите количество жителей " << i + 1 << ":";
+
+	cin >> A[i]-> kolvo;
+
+	cout << "Введите площадь " << i + 1 << ":";
+
+	cin >> A[i]->S;
 
 
-	return 0;
+}
+
+}
+ return 0;
 
 }
